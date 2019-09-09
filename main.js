c093e276ddde441ccc1b9175850d638703c26b03
tunm065 <tunguyenminh065@gmail.com>
2019-09-09 10:24:32 +0700

    start quill

--- main.js
+++ main.js
@@ -1,153 +1,55 @@
-const Player = require('./player.js')
-const $ = require('jquery')
-const signalhub = require('signalhub')
-const swarm = require('webrtc-swarm')
-//const canvas = require('./canvas')
-const hub = signalhub('my-game', [
-  'https://handshakesignalserver.herokuapp.com/'
-])
-const sw = swarm(hub, { 
-  //opts
+const Quill = require('quill')
+var toolbarOptions = [
+  [{ 'font': [] }],
+  [{ 'size': ['small', false, 'large', 'huge'] }],
+
+  ['bold', 'italic', 'underline', 'strike'],        
+
+  [{ 'color': [] }, { 'background': [] }],  
+
+  [{ 'script': 'sub'}, { 'script': 'super' }],      
+
+  [{ 'header': 1 }, { 'header': 2 }],              
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  ['blockquote', 'code-block'],
+
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'indent': '-1'}, { 'indent': '+1' }],        
+
+  [{ 'direction': 'rtl' }],                      
+  [{ 'align': [] }],
+    
+  ['link', 'image', 'video', 'formula'],       
+
+  ['clean']  
+]
+
+/*var toolbarOptions = [*/
+  //['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  //['blockquote', 'code-block'],
+
+  //[{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  //[{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  //[{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  //[{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  //[{ 'direction': 'rtl' }],                         // text direction
+    
+  //// custom dropdown
+  //[{ 'size': ['small', false, 'large', 'huge'] }],
+  //[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  //// dropdown with defaults from theme
+  //[{ 'color': [] }, { 'background': [] }],  
+  //[{ 'font': [] }],
+  //[{ 'align': [] }],
+
+  //// remove formatting button
+  //['clean']  
+/*]*/
+
+var quill = new Quill('#editor', {
+  modules: {
+    toolbar: toolbarOptions
+  },
+  theme: 'snow'
 })
-
-const canvas = document.querySelector('#canvas') 
-const context = canvas.getContext("2d")
-//Set canvas size
-canvas.height = window.innerHeight
-canvas.width = window.innerWidth
-//variables 
-let painting = false
-
-$( "#import" ).change(function() {
-  readURL(this)
-  })
-
-function readURL(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-
-    reader.onload = function(e) {
-      //$('#image').attr('src', e.target.result);
-      base_image = new Image()
-      base_image.src = e.target.result
-      base_image.onload = function(){
-        context.drawImage(base_image, 0, 0)
-      }
-    }
-
-    reader.readAsDataURL(input.files[0]);
-  }
-}
-
-window.addEventListener("load", () => {
-  navigator.mediaDevices.getUserMedia({ video: false, audio: true })
-  .then( localStream => {
-
-    const players = {}
-    const localPlayer = new Player()
-    localPlayer.addStream(localStream)
-    console.log('local stream')
-    console.log(localStream)
-
-    sw.on('connect', function (peer, id) {
-      if (!players[id]) {
-        players[id] = new Player()
-        peer.on('data', function (data) {
-          data = JSON.parse(data.toString())
-          if(data.color){
-            players[id].update(data)
-          }
-
-          updateCanvas(data)
-        })
-
-        peer.addStream(localStream)
-        peer.on('stream', stream => {
-          console.log('received stream from: ', id)
-          console.log(stream)
-          players[id].addStream(stream)
-        })
-      }
-    })
-
-    sw.on('disconnect', function (peer, id) {
-      if (players[id]) {
-        players[id].element.parentNode.removeChild(players[id].element)
-        delete players[id]
-      }
-    })
-
-    canvas.addEventListener('mousedown', startPosition)
-    canvas.addEventListener('mouseup', finishedPosition)
-    canvas.addEventListener('mousemove', draw)
-
-    document.addEventListener('keypress', event => {
-      const speed = 16
-      switch (event.key) {
-        case 'a':
-          localPlayer.x -= speed
-          break
-        case 'd':
-          localPlayer.x += speed
-          break
-        case 'w':
-          localPlayer.y -= speed
-          break
-        case 's':
-          localPlayer.y += speed
-          break
-      }
-
-      localPlayer.update()
-      const localPlayerString = JSON.stringify(localPlayer)
-      sw.peers.forEach(peer => {
-        peer.send(localPlayerString)
-      })
-
-    }, false)
-  })
-})
-function startPosition(e){
-    painting = true
-    draw(e)
-}
-
-function finishedPosition(){
-  painting = false
-  context.beginPath()
-  sw.peers.forEach(peer => {
-    peer.send(JSON.stringify({painting: painting}))
-  })
-}
-
-
-function draw(e){
-    if (!painting) return
-    context.lineWidth = 9
-    context.lineCap = "round"
-    context.strokeStyle = "black"
-
-    context.lineTo(e.clientX, e.clientY)
-    context.stroke()
-    context.beginPath()
-    context.moveTo(e.clientX, e.clientY)
-
-    sw.peers.forEach(peer => {
-    peer.send(JSON.stringify({painting: painting, x: e.clientX, y: e.clientY }))
-    })
-}
-
-function updateCanvas(data){
-  if (!data.painting){
-    context.beginPath()
-    return
-  } 
-  context.lineWidth = 9
-  context.lineCap = "round"
-  context.strokeStyle = "black"
-
-  context.lineTo(data.x, data.y)
-  context.stroke()
-  context.beginPath()
-  context.moveTo(data.x, data.y)
-}

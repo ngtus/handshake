1d965e5a2670b4a14b2d3e82e44abfa56c166d6c
tunm065 <tunguyenminh065@gmail.com>
2019-09-19 15:16:16 +0700

    revert back to http

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require('express')
 //const httpsLocalhost = require("https-localhost")
 
 //const app = httpsLocalhost()
-app.use(express.static('./'));
+const app = express()
+app.use(express.static('./'))
 
 app.listen(3000, () => console.log('Server started'))
 
